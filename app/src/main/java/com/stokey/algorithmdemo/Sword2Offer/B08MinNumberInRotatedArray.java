a9533d2e3d277e693df55fc512cc65a86c564a8b
stokey <stokeygen@hotmail.com>
2017-05-21 23:31:19 +0800

    feat: add Sward2Offer code about B08/09

--- app/src/main/java/com/stokey/algorithmdemo/Sword2Offer/B08MinNumberInRotatedArray.java
+++ app/src/main/java/com/stokey/algorithmdemo/Sword2Offer/B08MinNumberInRotatedArray.java
@@ -5,4 +5,96 @@ package com.stokey.algorithmdemo.Sword2Offer;
  */
 
 public class B08MinNumberInRotatedArray {
+    /**
+     * 排序时间复杂度O(n),空间复杂度不超过O(n)
+     * 待排序数据大小范围[0,k]
+     *
+     * @param input
+     * @param length
+     */
+    public void sort(int[] input, int length) {
+        // 输入合法性检查
+        if (input == null || input.length <= 0 || length <= 0) {
+            return;
+        }
+
+        final int numMax = 100; // k值
+        int[] numCountArray = new int[numMax + 1];// 记录不同数值出现的次数
+        for (int i = 0; i < numCountArray.length; i++) {
+            numCountArray[i] = 0;
+        }
+
+        for (int j = 0; j < input.length; j++) {
+            int num = input[j];
+            if (num < 0 || num > numMax) {
+                throw new RuntimeException("input data error,not in range");
+            }
+            ++numCountArray[num];
+        }
+
+        int index = 0;
+        // 输出已经记录num出现的次数
+        for (int i = 0; i < numMax; i++) {
+            int countNums = numCountArray[i];
+            if (countNums > 0) {
+                for (int j = 0; j < countNums; j++) {
+                    input[index] = i;
+                    ++index;
+                }
+            }
+        }
+    }
+
+    /**
+     * 旋转数组的最小数字
+     * 二分查找
+     *
+     * @param input
+     * @param length
+     * @return
+     */
+    public int findMin(int[] input, int length) {
+        // TODO: 检查输入合法性
+        int left = 0, right = length - 1;
+        int midIndex = 0;
+        while (input[left] >= input[right]) {
+            if (right - left == 1) {
+                midIndex = right;
+                break;
+            }
+            midIndex = left + (right - left) / 2;
+            // 下标为left,mid,left指向三个数字相等时，则只能顺序查找
+            if (input[left] == input[right] && input[midIndex] == input[left]) {
+                return orderFind(input, left, right);
+            }
+            if (input[midIndex] >= input[left]) {
+                left = midIndex;
+            } else if (input[midIndex] <= input[right]) {
+                right = midIndex;
+            }
+        }
+
+        return input[midIndex];
+    }
+
+    /**
+     * 按顺序查找出第一个打乱顺序的元素
+     * [1,0,1,1,1]
+     * [1,1,1,0,1]
+     *
+     * @param input
+     * @param left
+     * @param right
+     * @return
+     */
+    private int orderFind(int[] input, int left, int right) {
+        // TODO: 检查输入合法性
+        int result = input[left];
+        for (int i = left + 1; i <= right; i++) {
+            if (result > input[i]) {
+                result = input[i];
+            }
+        }
+        return result;
+    }
 }
---
+++ app/src/main/java/com/stokey/algorithmdemo/Sword2Offer/B09Fibonacci.java
@@ -0,0 +1,69 @@
+package com.stokey.algorithmdemo.Sword2Offer;
+
+/**
+ * Created by stokey on 2017/5/21.
+ */
+
+public class B09Fibonacci {
+
+    /**
+     * 【无法获得offer】
+     * 最原始的递归，有严重的效率问题
+     * f(10) = f(9)+f(8)
+     * f(9) = f(8)+f(7)
+     * 存在多次重复计算
+     *
+     * @param n
+     * @return
+     */
+    public static long fibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    /**
+     * 改进后，时间复杂度O(n)
+     * @param n
+     * @return
+     */
+    public static long fibonacciAdvance1(int n) {
+        // 存放需要f(n-1),f(n-2)的值
+        long[] result = {0, 1};
+        if (n < 2) {
+            return result[n];
+        }
+
+        long fibOne = result[0];
+        long fibTwo = result[1];
+        long fibN = 0;
+        for (int i = 2; i <= n; i++) {
+            fibN = fibOne + fibTwo;
+            fibOne = fibTwo;
+            fibTwo = fibN;
+        }
+        return fibN;
+    }
+
+    /**
+     * 最优解，时间复杂度O(nlogn)
+     * 使用到矩阵
+     * @param n
+     * @return
+     */
+    public long fibonacciAdvance2(int n){
+        // 未实现
+        if(n <= 0){
+            return 0;
+        }
+        if (n==1){
+            return 1;
+        }
+
+        return -1;
+    }
+}
